b25182b7fe10f9317150b554e475fbaa3a64e11a
Josip Katalinic <josip-kuti@hotmail.com>
2014-07-19 00:17:19 +0200

    Završen trinaesti tutorial video

--- Assets/Code/CharacterController2D.cs
+++ Assets/Code/CharacterController2D.cs
@@ -120,6 +120,8 @@ public class CharacterController2D : MonoBehaviour
 
             MoveVertically(ref deltaMovement);
 
+            CorrectHorizontalPlacement(ref deltaMovement, true);
+            CorrectHorizontalPlacement(ref deltaMovement, false);
         }
 
         _transform.Translate(deltaMovement, Space.World);
@@ -176,6 +178,34 @@ public class CharacterController2D : MonoBehaviour
             PlatformVelocity = Vector3.zero;
 
         StandingOn = null;
+    }
+
+    private void CorrectHorizontalPlacement(ref Vector2 deltaMovement, bool isRight)
+    {
+        var halfWidth = (_boxCollider.size.x * _localScale.x) / 2f;
+        var rayOrigin = isRight ? _raycastBottomRight : _raycastBottomLeft;
+
+        if (isRight)
+            rayOrigin.x -= (halfWidth - SkinWidth);
+        else
+            rayOrigin.x += (halfWidth - SkinWidth);
+
+        var rayDirection = isRight ? Vector2.right : -Vector2.right;
+        var offset = 0f;
+
+        for (var i = 1; i < TotalHorizontalRays - 1; i++)
+        {
+            var rayVector = new Vector2(deltaMovement.x + rayOrigin.x, deltaMovement.y + rayOrigin.y + (i * _verticalDistanceBetweenRays));
+            // Debug.DrawRay(rayVector, rayDirection * halfWidth, isRight ? Color.cyan : Color.magenta);
+
+            var rayCastHit = Physics2D.Raycast(rayVector, rayDirection, halfWidth, PlatformMask);
+            if (!rayCastHit)
+                continue;
+
+            offset = isRight ? ((rayCastHit.point.x - _transform.position.x) - halfWidth) : (halfWidth - (_transform.position.x - rayCastHit.point.x));
+        }
+
+        deltaMovement.x += offset;
     }
 
     private void CalculateRayOrigins()
