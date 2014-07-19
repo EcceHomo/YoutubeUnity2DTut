689e385e01233bb3970e0f09fde0f0397ae42134
Josip Katalinic <josip-kuti@hotmail.com>
2014-07-19 05:23:50 +0200

    Završen osamnaesti tutorial video

---
+++ Assets/Code/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class Checkpoint : MonoBehaviour
+{
+    public void Start()
+    {
+    }
+
+    public void PlayerHitCheckpoint()
+    {
+    }
+
+    private IEnumerator PlayerHitCheckpointCo(int bonus)
+    {
+        yield break;
+    }
+
+    public void PlayerLeftCheckpoint()
+    {
+    }
+
+    public void SpawnPlayer(Player player)
+    {
+    }
+
+    public void AssignObjectToCheckpoint()
+    {
+    }
+}
---
+++ Assets/Code/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    public Player Player { get; private set; }
+    public CameraController Camera { get; private set; }
+
+    public void Awake()
+    {
+
+    }
+
+    public void Start()
+    {
+    }
+
+    public void KilPlayer()
+    {
+    }
+
+    private IEnumerator KillPlayerCo()
+    {
+        yield break;
+    }
+
+}
