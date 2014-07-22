e8352410bb434ccbb0e361ea3d00a2945e19113d
Josip Katalinic <josip-kuti@hotmail.com>
2014-07-22 18:01:02 +0200

    Završen trideset četvrti tutorial video

--- Assets/Code/PointStar.cs
+++ Assets/Code/PointStar.cs
@@ -5,9 +5,16 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener
     public GameObject Effect;
     public int PointsToAdd = 10;
     public AudioClip HitStarSound;
+    public Animator Animator;
+    public SpriteRenderer Renderer;
+
+    private bool _isCollected;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if(_isCollected)
+            return;
+
         if (other.GetComponent<Player>() == null)
             return;
 
@@ -16,15 +23,23 @@ public class PointStar : MonoBehaviour, IPlayerRespawnListener
 
         GameManager.Instance.AddPoints(PointsToAdd);
         Instantiate(Effect, transform.position, transform.rotation);
-
-        gameObject.SetActive(false);
-
         FloatingText.Show(string.Format("+{0}!", PointsToAdd), "PointStarText", new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
+
+        _isCollected = true;
+        Animator.SetTrigger("Collect");
+
+
     }
 
+    public void FinishAnimationEvent()
+    {
+        Renderer.enabled = false;
+        Animator.SetTrigger("Reset");
+    }
 
     public void OnPlayerRespawnInThisCheckpoint(Checkpoint checkpoint, Player player)
     {
         gameObject.SetActive(true);
+        Renderer.enabled = true;
     }
 }
